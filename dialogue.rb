22bef09410b97b35acfb493ece358c4f961ed184
debbie <drosenfeld87@gmail.com>
2018-02-14 14:59:39 -0500

    complete erb basics

--- dialogue.rb
+++ dialogue.rb
@@ -4,14 +4,14 @@ price = 8.75
 
 line_1 = "Bill: That will cost you $<%= price %> plus taxes, ma'am."
 line_2 = "Pritma: How much is it with taxes?"
-line_3 = "Bill: I don't know! I'm not very good at math."
+line_3 = "Bill:   That will be $<%= (price * 1.13).round(2) %> please."
 
 line_1_erb_instance = ERB.new(line_1)
 
 puts
-puts line_1_erb_instance.result
+puts ERB.new(line_1).result
 puts
 puts line_2
 puts
-puts line_3
+puts ERB.new(line_3).result
 puts
